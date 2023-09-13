d62179aa2b5c47c73b9ffd42ce41a44cafb60fd2
Ololade <37458073+olaonipekun@users.noreply.github.com>
2023-09-13 02:50:30 +0000

    commit and update

--- moviedir/src/App.js
+++ moviedir/src/App.js
@@ -4,7 +4,7 @@ import Header from './component/Header';
 
 function App() {
   return (
-    <div className="App container-fluid dark:bg-slate-800 dark:text-white">
+    <div className="App dark:bg-slate-800 dark:text-white grid grid-cols-1 grid-rows-1">
       <Header />
       <Footer />
     </div>
