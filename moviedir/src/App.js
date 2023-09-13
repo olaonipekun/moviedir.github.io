98dcdaa91d314ba40f836780e343ad178b2e1c12
Ololade <37458073+olaonipekun@users.noreply.github.com>
2023-09-13 02:47:25 +0000

    commit

--- moviedir/src/App.js
+++ moviedir/src/App.js
@@ -4,7 +4,7 @@ import Header from './component/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container-fluid">
       <Header />
       <Footer />
     </div>
