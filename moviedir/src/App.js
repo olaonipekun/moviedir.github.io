e6c4986b205ae6e0b8ed174dc792d4d063c4c12f
Ololade <37458073+olaonipekun@users.noreply.github.com>
2023-09-13 03:31:14 +0000

    another commit

--- moviedir/src/App.js
+++ moviedir/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Footer from './component/Footer';
 import Header from './component/Header';
+import Body from './component/Body'
 
 function App() {
   return (
     <div className="App">
       <div className='container mx-auto h-screen w-screen dark:bg-slate-800 dark:text-white grid grid-cols-1 grid-rows-1'>
         <Header />
+        <Body />
         <Footer />
       </div>
       
