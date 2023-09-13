a93054a8e06ff4a63b58d1d304df1e15bc30a7bf
Ololade <37458073+olaonipekun@users.noreply.github.com>
2023-09-13 03:47:00 +0000

    commit

--- moviedir/src/App.js
+++ moviedir/src/App.js
@@ -5,7 +5,7 @@ import Body from './component/Body'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container-fluid">
       <div className=' grid grid-cols-1 mx-auto dark:bg-slate-800 dark:text-white'>
         <Header />
         <Body />
